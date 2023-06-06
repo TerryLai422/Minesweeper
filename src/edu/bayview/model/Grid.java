09097290e903e2258497b6fcc3643273ef7eb00b
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2023-06-07 07:14:25 +0800

    Update Grid.java
    grid

--- src/edu/bayview/model/Grid.java
+++ src/edu/bayview/model/Grid.java
@@ -4,5 +4,8 @@ package edu.bayview.model;
  * The underlying model for each grid
  */
 public class Grid {
-
+    private boolean revealed;
+    private boolean hasBomb;
+    private boolean flagged;
+    private int numBombsAround;
 }
