d95a678f178f44554f392f296e25f25c5d1557dc
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2023-06-13 01:10:42 +0800

    remove unused import statements

--- src/edu/bayview/view/BoardView.java
+++ src/edu/bayview/view/BoardView.java
@@ -1,9 +1,6 @@
 package edu.bayview.view;
 
 import java.awt.BorderLayout;
-import java.util.Timer;
-import java.util.TimerTask;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
